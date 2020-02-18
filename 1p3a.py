6dee16f117dd08090b68ee32e6977abcdea1d3ab
mw <346454127@qq.com>
2020-02-18 03:17:48 -0600

    3rd

--- 1p3a.py
+++ 1p3a.py
@@ -4,6 +4,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
 import time
+import logging
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+import datetime
+datetime_object = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+def printAndLogging(s):
+    print(s)
+    logging.debug(s)
+printAndLogging(datetime_object)
 with open("accounts.json") as json_file:
     data = json.load(json_file)
     account = data['account']
@@ -11,9 +19,9 @@ with open("accounts.json") as json_file:
 options = Options()
 ua = UserAgent()
 userAgent = ua.random
-print(userAgent)
+printAndLogging(userAgent)
 options.add_argument('user-agent=' + userAgent)
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(options=options)
 driver.get("https://www.1point3acres.com/bbs")
 driver.implicitly_wait(3)
 account_input = driver.find_element_by_name("username")
@@ -23,30 +31,26 @@ pass_input.send_keys(password)
 pass_input.send_keys(Keys.ENTER)
 time.sleep(2)
 driver.get("https://www.1point3acres.com/bbs")
-time.sleep(5)
+time.sleep(3)
 attachment = 'image.png'
 driver.save_screenshot(attachment)
 point = driver.find_element_by_id("extcreditmenu").text
-print('old point', point)
+printAndLogging('old point' + point)
 from selenium.webdriver.common.by import By
-#newPoint = point
-#needSignOn = True
 signNodes = driver.find_elements(By.XPATH, "//*[@id='um']/p[2]/a[3]/font[text()='签到领奖!']")
 if len(signNodes) > 0:
     signNode = signNodes[0]
     signNode.send_keys(Keys.ENTER)
-    time.sleep(5)
+    time.sleep(3)
     driver.find_element(By.XPATH, "//*[@id='kx']").click()
-    import datetime
-    datetime_object = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     signText = '今天把论坛帖子介绍给好基友了~'
-    print(signText)
+    printAndLogging(signText)
     driver.find_element(By.XPATH, "//*[@id='todaysay']").send_keys(signText)
     driver.find_element(By.XPATH, "//*[@id='qiandao']/p/button").click()
-    time.sleep(5)
+    time.sleep(3)
     newPoint = driver.find_element_by_id("extcreditmenu").text
     pointChange = point + ' -> ' + newPoint + ' : ' + datetime_object
-    print('new point', newPoint)
+    printAndLogging('new point' + newPoint)
     import smtplib, ssl
     from email.mime.text import MIMEText
     from email.mime.multipart import MIMEMultipart
@@ -68,4 +72,6 @@ if len(signNodes) > 0:
         img.add_header('Content-ID', '<{}>'.format(attachment))
         message.attach(img)
         server.sendmail(account, receiver, message.as_string())
-	
+printAndLogging('Finished successfully!\n\n\n')
+time.sleep(1)
+driver.quit()
